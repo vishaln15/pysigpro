33be3255fe688b984d179957b3ea3010f6220418
vishaln15 <nagarajanvishal@gmail.com>
2021-10-07 19:44:39 +0530

    Added entropy_features, frequency_domain_features

--- eeg/features.py
+++ eeg/features.py
@@ -1,17 +1,22 @@
 import numpy as np
+import pandas as pd
 from numba import jit
 import scipy
 import statistics
-import utils
+from utils import *
 
 
-__all__ = ['get_time_domain_features', 'get_entropy_features']
+__all__ = ['get_time_domain_features', 'get_entropy_features', 'get_frequency_domain_features']
 
-def get_time_domain_features(x):
+def get_time_domain_features(x: list):
 
+    assert x is not None, "Invalid signal provided"
+
+    x = np.asarray(x)
     mean = np.mean(x)
     stddev = np.stddev(x)
     kurtosis = scipy.stats.kurtosis(x)
+    skewness = scipy.stats.skew(x)
     variance = np.var(x)
     mode = statistics.mode(x)
     median = statistics.median(x)
@@ -21,12 +26,13 @@ def get_time_domain_features(x):
     rms = np.sqrt(np.mean(x**2))
     hjorthActivity = variance
     hjorthMobility = np.sqrt(np.var(np.gradient(x)) / np.var(x))
-    hjorthComplexity = utils.hMob(np.gradient(x)) / utils.hMob(x)
+    hjorthComplexity = hMob(np.gradient(x)) / hMob(x)
 
     time_domain_features = {
         'mean' : mean,
         'stddev' : stddev,
         'kurtosis' : kurtosis,
+        'skewness' : skewness,
         'variance' : variance,
         'mode' : mode,
         'median' : median,
@@ -40,3 +46,61 @@ def get_time_domain_features(x):
     }
 
     return time_domain_features
+
+def get_entropy_features(x: list, order = 3, metric = 'chebyshev'):
+
+    assert x is not None, "Invalid signal provided"
+
+    x = np.asarray(x)
+    shannon_entropy = scipy.stats.entropy(pd.Series(x).value_counts())
+    phi = app_samp_entropy(x, order=order, approximate=True)
+    approximate_entropy = np.subtract(phi[0], phi[1])
+
+    if metric == 'chebyshev' and x.size < 5000:
+        sample_entropy = _numba_sampen(x, order=order, r=(0.2 * x.std(ddof=0)))
+    else:
+        phi = app_samp_entropy(x, order=order, metric=metric,
+                                approximate=False)
+        sample_entropy = -np.log(np.divide(phi[1], phi[0]))
+
+    entropy_features = {
+        'shannon_entropy' : shannon_entropy,
+        'approximate_entropy' : approximate_entropy,
+        'sample_entropy' : sample_entropy,
+    }
+
+    return entropy_features
+
+def get_frequency_domain_features(x: list, band = None, fs = None):
+
+    assert x is not None, "Invalid signal provided"
+    assert fs is not None, "Sampled Frequency must be provided."
+
+    fs = float(fs)
+
+    x = abs(np.fft.fft(x))
+    power = np.empty(len(band) - 1)
+    
+    if band is None:
+        band = [0.5, 4, 7, 12, 30]
+    
+    try:
+        for index in range(0, len(band) - 1):
+            curr_freq = float(band[index])
+            next_freq = float(band[index + 1])
+            power[index] = sum(
+                x[int(np.floor(curr_freq / fs * len(x))): 
+                    int(np.floor(next_freq / fs * len(x)))]
+            )
+
+    except ValueError:
+        raise ValueError("Incorrect Sampling Frequency")
+
+    power_ratio = power / sum(power)
+
+    frequency_features = {
+        'power' : power,
+        'power_ratio': power_ratio
+    }
+
+    return frequency_features
--- eeg/utils.py
+++ eeg/utils.py
@@ -1,5 +1,120 @@
 import numpy as np
+from sklearn.neighbors import KDTree
+from numba import jit
+from math import *
 
 def hMob(x):
     row = np.array(x)
     return (np.sqrt(np.var(np.gradient(x)) / np.var(x)))
+
+def embed(x, order=3, delay=1):
+    """Time-delay embedding.
+    Parameters
+    ----------
+    x : 1d-array
+        Time series, of shape (n_times)
+    order : int
+        Embedding dimension (order).
+    delay : int
+        Delay.
+    Returns
+    -------
+    embedded : ndarray
+        Embedded time-series, of shape (n_times - (order - 1) * delay, order)
+    """
+    N = len(x)
+    if order * delay > N:
+        raise ValueError("Error: order * delay should be lower than x.size")
+    if delay < 1:
+        raise ValueError("Delay has to be at least 1.")
+    if order < 2:
+        raise ValueError("Order has to be at least 2.")
+    Y = np.zeros((order, N - (order - 1) * delay))
+    for i in range(order):
+        Y[i] = x[(i * delay):(i * delay + Y.shape[1])]
+    return Y.T
+
+def app_samp_entropy(x, order, metric='chebyshev', approximate=True):
+    
+    _all_metrics = KDTree.valid_metrics
+    if metric not in _all_metrics:
+        raise ValueError('The given metric (%s) is not valid. The valid '
+                         'metric names are: %s' % (metric, _all_metrics))
+    phi = np.zeros(2)
+    r = 0.2 * np.std(x, axis=-1, ddof=1)
+
+    # compute phi(order, r)
+    _emb_data1 = embed(x, order, 1)
+    if approximate:
+        emb_data1 = _emb_data1
+    else:
+        emb_data1 = _emb_data1[:-1]
+    count1 = KDTree(emb_data1, metric=metric).query_radius(emb_data1, r,
+                                                           count_only=True
+                                                           ).astype(np.float64)
+    # compute phi(order + 1, r)
+    emb_data2 = embed(x, order + 1, 1)
+    count2 = KDTree(emb_data2, metric=metric).query_radius(emb_data2, r,
+                                                           count_only=True
+                                                           ).astype(np.float64)
+    if approximate:
+        phi[0] = np.mean(np.log(count1 / emb_data1.shape[0]))
+        phi[1] = np.mean(np.log(count2 / emb_data2.shape[0]))
+    else:
+        phi[0] = np.mean((count1 - 1) / (emb_data1.shape[0] - 1))
+        phi[1] = np.mean((count2 - 1) / (emb_data2.shape[0] - 1))
+    return phi
+
+@jit('f8(f8[:], i4, f8)', nopython=True)
+def _numba_sampen(x, order, r):
+    """
+    Fast evaluation of the sample entropy using Numba.
+    """
+    n = x.size
+    n1 = n - 1
+    order += 1
+    order_dbld = 2 * order
+
+    # Define threshold
+    # r *= x.std()
+
+    # initialize the lists
+    run = [0] * n
+    run1 = run[:]
+    r1 = [0] * (n * order_dbld)
+    a = [0] * order
+    b = a[:]
+    p = a[:]
+
+    for i in range(n1):
+        nj = n1 - i
+
+        for jj in range(nj):
+            j = jj + i + 1
+            if abs(x[j] - x[i]) < r:
+                run[jj] = run1[jj] + 1
+                m1 = order if order < run[jj] else run[jj]
+                for m in range(m1):
+                    a[m] += 1
+                    if j < n1:
+                        b[m] += 1
+            else:
+                run[jj] = 0
+        for j in range(order_dbld):
+            run1[j] = run[j]
+            r1[i + n * j] = run[j]
+        if nj > order_dbld - 1:
+            for j in range(order_dbld, nj):
+                run1[j] = run[j]
+
+    m = order - 1
+
+    while m > 0:
+        b[m] = b[m - 1]
+        m -= 1
+
+    b[0] = n * n1 / 2
+    a = np.array([float(aa) for aa in a])
+    b = np.array([float(bb) for bb in b])
+    p = np.true_divide(a, b)
+    return -log(p[-1])
