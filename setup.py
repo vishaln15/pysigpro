26e76bdb78e048c7a4f9ab45b2e38e73e6f5ebbc
vishaln15 <nagarajanvishal@gmail.com>
2021-11-18 12:29:34 +0530

    Test setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = '0.0.2'
+version = '0.0.3'
 
 setuptools.setup(
     name='pysigpro',
@@ -13,7 +13,7 @@ setuptools.setup(
     description='ECG/EEG Signal Feature Extraction Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/vishaln15/pysigpro',
+    url='https://github.com/vishaln15/pysigpro/pysigpro',
     project_urls = {
         "Bug Tracker": "https://github.com/vishaln15/pysigpro/issues"
     },
