cf5af06bf96476c88eb9d15815750843c629ddc7
vishaln15 <nagarajanvishal@gmail.com>
2021-11-18 13:23:16 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description='ECG/EEG Signal Feature Extraction Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/vishaln15/pysigpro/pysigpro',
+    url='https://github.com/vishaln15/pysigpro',
     project_urls = {
         "Bug Tracker": "https://github.com/vishaln15/pysigpro/issues"
     },
